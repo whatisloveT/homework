bb3a0f39591026b30b26d8dcca27048a0b862e31
Vladislav <whatislove56@icloud.com>
2021-08-07 15:32:38 +0500

    добавлены файлы с решениями заданий второго урока

--- homework_python/lesson_one_task_two.py
+++ homework_python/lesson_one_task_two.py
@@ -1,3 +1,5 @@
+from typing import Any, Union
+
 list_number = []
 final_sum_one = 0
 final_sum_two = 0
@@ -6,17 +8,21 @@ for i in range(1, 1001):
         list_number.append(i**3)
 print(list_number)
 for i in list_number:
+    x = i
     sum_number = 0
-    for x in str(i):
-        sum_number += int(x)
+    while i != 0:
+        sum_number += i % 10
+        i = i // 10
     if sum_number % 7 == 0:
-        final_sum_one += i
+        final_sum_one += x
     sum_number = 0
-    i += 17
-    for x in str(i):
-        sum_number += int(x)
+    x += 17
+    i = x
+    while i != 0:
+        sum_number += i % 10
+        i = i // 10
     if sum_number % 7 == 0:
-        final_sum_two += i
+        final_sum_two += x
 
 print(f'Сумма чисел вариант (а)  = {final_sum_one}')
 print(f'Сумма чисел вариант (b)  = {final_sum_two}')
